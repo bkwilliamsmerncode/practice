92218e2a6eeda5453519a0833f208751468ff9b1
Brian Williams <152632135+bkwilliamsmerncode@users.noreply.github.com>
2025-03-20 21:02:49 -0700

    3/20/2025

---
+++ practice.js
@@ -0,0 +1,147 @@
+///////////////////////////////////////////////////////////////////// EDIBITS ////////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////// Less Than Or Equal To Zero /////////////////////////////////////////////////////////////////////////
+
+// function lessThanOrEqualToZero(num) {
+// 	if(num <= 0) {
+//         return "true"
+//     } else {
+//         return "false"
+//     }
+// }
+
+// lessThanOrEqualToZero(5) ➞ false
+
+// lessThanOrEqualToZero(0) ➞ true
+
+// lessThanOrEqualToZero(-2) ➞ true
+
+// console.log(lessThanOrEqualToZero(5))
+
+// console.log(lessThanOrEqualToZero(0))
+
+// console.log(lessThanOrEqualToZero(-2))
+
+////////////////////////////////////// Return the First Element in an Array//////////////////////////////////////
+
+// function getFirstValue(arr) {
+// 	let result = arr.shift();
+//     return result
+// }
+
+// getFirstValue([1, 2, 3]) ➞ 1
+
+// getFirstValue([80, 5, 100]) ➞ 80
+
+// getFirstValue([-500, 0, 50]) ➞ -500
+
+// console.log(getFirstValue([1, 2, 3]))
+
+// console.log(getFirstValue([80, 5, 100]))
+
+// console.log(getFirstValue([-500, 0, 50]))
+
+////////////////////////////////////// Return Something to Me! //////////////////////////////////////////////////////
+
+// function giveMeSomething(a) {
+// 	return "something" + " " + a;
+// }
+
+// giveMeSomething("is better than nothing") ➞ "something is better than nothing"
+
+// giveMeSomething("Bob Jane") ➞ "something Bob Jane"
+
+// giveMeSomething("something") ➞ "something something"
+
+// console.log(giveMeSomething("is better than nothing")) 
+
+// console.log(giveMeSomething("Bob Jane")) 
+
+// console.log(giveMeSomething("something")) 
+
+////////////////////////////////////////////////////////////////////////// Basketball Points ///////////////////////////////////////////////////////////////////////
+
+// function points(twoPointers, threePointers) {
+// 	return twoPointers * 2 + threePointers * 3;
+// }
+
+// points(1, 1) ➞ 5
+
+// points(7, 5) ➞ 29
+
+// points(38, 8) ➞ 100
+
+// console.log(points(1, 1))
+
+// console.log(points(7, 5))
+
+// console.log(points(38, 8))
+
+////////////////////////////////////////////////////////////////// Less Than 100? //////////////////////////////////////////////////////////////////////////
+
+// function lessThan100(a, b) {
+// 	if( a + b < 100) {
+//         return true 
+//     } else {
+//         return false
+//     }
+// }
+
+//lessThan100(22, 15) ➞ true 
+// 22 + 15 = 37
+
+//lessThan100(83, 34) ➞ false
+// 83 + 34 = 117
+
+//lessThan100(3, 77) ➞ true
+
+// console.log(lessThan100(22, 15))
+
+// console.log(lessThan100(83, 34))
+
+// console.log(lessThan100(3, 77))
+
+/////////////////////////////////////////////////////////////////// The Farm Problem //////////////////////////////////////////////////////////////////
+
+// function animals(chickens, cows, pigs) {
+// 	return chickens * 2 + cows * 4 + pigs * 4;
+// }
+
+// animals(2, 3, 5) ➞ 36
+
+// animals(1, 2, 3) ➞ 22
+
+// animals(5, 2, 8) ➞ 50
+
+// console.log(animals(2, 3, 5))
+
+// console.log(animals(1, 2, 3))
+
+// console.log(animals(5, 2, 8))
+
+///////////////////////////////////////////////////////////////// Using the "&&" Operator ////////////////////////////////////////////////////////////
+
+// function and(a, b) {
+// 	if( a == true && b == true) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// and(true, false) ➞ false
+
+// and(true, true) ➞ true
+
+// and(false, true) ➞ false
+
+// and(false, false) ➞ false
+
+// console.log(and(true, false))
+
+// console.log(and(true, true))
+
+// console.log(and(false, true))
+
+// console.log(and(false, false))
